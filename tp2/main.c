553126ec2f797a7790cc82bc4eb20f8c2855b6fb
Jordan Moreira <97352446+jordan-moreira@users.noreply.github.com>
2023-11-21 17:34:07 -0300

    trabalho 1

---
+++ tp2/main.c
@@ -0,0 +1,10 @@
+
+
+//receber a entrada e criar uma matriz com os dados no formato a seguir
+//resistor    0 | 1| 2|
+//posicao0    tn|tn|tn|
+//posicao1    tn|tn|tn|
+//resistencia n | n| n|
+
+//resolver as paralela 
+//criar a matriz com os dados restantes
